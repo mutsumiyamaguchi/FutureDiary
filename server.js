580d20feb38058c6753ea41f962c6aa8a4ac2e1f
mutsumiyamaguchi <tayomuwa2020@gmail.com>
2025-04-05 15:10:12 +0900

    add server

---
+++ server.js
@@ -0,0 +1,16 @@
+onst express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// ミドルウェア
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// サーバー起動
+app.listen(PORT,"localhost", () => {
+  console.log(`Server running on port ${PORT}`);
+});
