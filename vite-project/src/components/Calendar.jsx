26a3e2b7d644911fc1ba4e251f3287bf56bdf20f
高野友博 <tt0628s@icloud.com>
2025-04-06 14:42:55 +0900

    insert task time

--- vite-project/src/components/Calendar.jsx
+++ vite-project/src/components/Calendar.jsx
@@ -1,20 +1,20 @@
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import Counter from './Counter';
-import jaLocale from '@fullcalendar/core/locales/ja'
-import { useEffect, useState } from 'react';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import Counter from "./Counter";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import { useEffect, useState } from "react";
 
 export default function Calendar({ onDateClick }) {
   const handleDateClick = (arg) => {
     // arg.dateStr: "2025-04-05" のような形式
     if (onDateClick) {
       onDateClick(arg.dateStr);
-      console.log(arg)
+      console.log(arg);
     }
   };
 
-  const API_URL = "http://localhost:5000/items"
+  const API_URL = "http://localhost:5000/items";
 
   const [events, setEvents] = useState([]);
 
@@ -23,32 +23,43 @@ export default function Calendar({ onDateClick }) {
       try {
         const response = await fetch(API_URL);
         const data = await response.json();
-        
-        console.log(data)
-        // データをカレンダー用の形に変換する
-        const formattedEvents = data.map(item => ({
-          title: item.name,    // ← ここはAPIの項目に合わせてね！
-          date: item.date,       // ← 日付もAPIの項目に合わせる
-        }));
+
+        console.log(data);
+        // ここでカレンダー用イベントに変換
+        const formattedEvents = data.map((item) => {
+          const event = {
+            title: item.name,
+            start: `${item.date}T${item.Time}`, // ← ここで start 作ってる
+          };
+          return event;
+        });
         setEvents(formattedEvents);
       } catch (error) {
         console.error("データ取得エラー:", error);
       }
     };
-  
+
     fetchList();
   }, []);
 
   return (
     <>
-    <Counter/>
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      locale={jaLocale}
-      dateClick={handleDateClick}
-      events={events}
-    />
+      <Counter />
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        locale={jaLocale}
+        dateClick={handleDateClick}
+        events={events}
+        // eventContent={(arg) => {
+        //     return (
+        //       <div>
+        //         <i>{arg.timeText}~</i><br />
+        //         <b>{arg.event.title}</b>
+        //       </div>
+        //     );
+        //   }}
+      />
     </>
   );
 }
